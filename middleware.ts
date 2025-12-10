6fa114d236c2fea44a76787ea7f47565085dcdd6
Dumitru-radu <101594913+radu9120@users.noreply.github.com>
2025-12-10 15:18:35 +0000

    refactor: update middleware to exclude sitemap.xml and robots.txt from request matching

--- middleware.ts
+++ middleware.ts
@@ -12,8 +12,10 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
+     * - sitemap.xml (SEO sitemap)
+     * - robots.txt (SEO robots)
      * Feel free to modify this pattern to include more paths.
      */
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
