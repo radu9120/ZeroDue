43f55e2263c6d4de0855295f0f95a4a6fe3cd4f7
Dumitru-radu <101594913+radu9120@users.noreply.github.com>
2025-12-14 10:22:26 +0000

    refactor: integrate Google Tag Manager script for improved tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -98,11 +98,9 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-<<<<<<< Updated upstream
         <OrganizationSchema />
         <SoftwareApplicationSchema />
         <WebsiteSchema />
-=======
         {/* Google Tag Manager */}
         <Script id="gtm-script" strategy="afterInteractive">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -111,7 +109,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-TGMZ7GXK');`}
         </Script>
->>>>>>> Stashed changes
       </head>
       <body
         className={`${inter.className} bg-white dark:bg-slate-900 transition-colors`}
